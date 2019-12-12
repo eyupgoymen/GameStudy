1b3d3c59b6bad88d9f2c51b61a68d94d311ac611
Eyup Kazım Göymen <eyup.goymen@appcent.mobi>
2019-12-12 13:50:30 +0300

    Initial project setup

---
+++ GameStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameStudy
+//
+//  Created by Eyup Kazım Göymen on 12.12.2019.
+//  Copyright © 2019 Eyup Kazım Göymen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GameStudyTests/GameStudyTests.swift
@@ -0,0 +1,34 @@
+//
+//  GameStudyTests.swift
+//  GameStudyTests
+//
+//  Created by Eyup Kazım Göymen on 12.12.2019.
+//  Copyright © 2019 Eyup Kazım Göymen. All rights reserved.
+//
+
+import XCTest
+@testable import GameStudy
+
+class GameStudyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
